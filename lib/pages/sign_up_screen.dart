261d5d8e79b08f1c3905a51ebf58e071981edc1c
Godawela <godawelaus.21@uom.lk>
2025-02-23 12:02:44 +0530

    12/02

--- lib/pages/sign_up_screen.dart
+++ lib/pages/sign_up_screen.dart
@@ -1,5 +1,4 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
